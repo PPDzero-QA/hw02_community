9a5ea66c54863d8251c9dffa11f93bd6d30a7bf3
Ruslan Sabirzyanov <dota.buy@yandex.ru>
2022-04-16 16:50:40 +0300

    Fix failed tests

--- yatube/posts/models.py
+++ yatube/posts/models.py
@@ -10,12 +10,13 @@ class Group(models.Model):
     title = models.CharField(
         max_length=200,
         verbose_name="Название группы"
-        )
+    )
     description = models.TextField(verbose_name="Описание группы")
     slug = models.SlugField(
-        primary_key=True,    # При изменении ломается все, я не знаю как исправить ошибки. Оставил как есть
+        primary_key=True,   # При изменении ломается все, я не знаю как исправить ошибки.
+                            # Оставил как есть
         verbose_name="Индетификатор группы"
-        )
+    )
 
     def __str__(self):
         return self.title
@@ -28,7 +29,7 @@ class Post(models.Model):
     pub_date = models.DateTimeField(
         auto_now_add=True,
         verbose_name="Дата публикации поста"
-        )
+    )
     author = models.ForeignKey(
         User,
         on_delete=models.CASCADE,
@@ -41,7 +42,8 @@ class Post(models.Model):
         null=True,
         on_delete=models.SET_NULL,
         related_name='posts',
-        verbose_name="Группа")
+        verbose_name="Группа"
+    )
 
     class Meta:
         ordering = ('-pub_date',)
